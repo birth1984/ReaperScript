87ad3f631b8b0ea2630fe3a7d70dc2bb52451491
Birth1984 <88521762@qq.com>
2023-12-13 00:03:42 +0800

    Create BirthFat_SpacePlay.lua
    空格键：
    如果有选中的Items  则只播放一次选中item
    如果没有选中Items  则正常播放

---
+++ BirthFat_SpacePlay.lua
@@ -0,0 +1,27 @@
+
+--## if there has item selected ,then play selected items once , else then play or stop  
+reaper.PreventUIRefresh(1)
+
+reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+
+reaper.ClearConsole()
+
+--reaper.ShowConsoleMsg(reaper.NamedCommandLookup("40044").." " ..tostring(reaper.Audio_IsRunning()))
+count_sel_items = reaper.CountSelectedMediaItems(0)
+if count_sel_items > 0 then
+  reaper.Main_OnCommand(reaper.NamedCommandLookup("_XENAKIOS_TIMERTEST1") , 0)
+else 
+  isRunning = reaper.Audio_IsRunning()
+  if isRunning == 1.0 then
+    reaper.Main_OnCommand(reaper.NamedCommandLookup("40044"), 0)
+  else 
+    reaper.Main_OnCommand(reaper.NamedCommandLookup("40044"), 1)
+  end
+
+end 
+
+reaper.Undo_EndBlock("space play", -1) -- End of the undo block. Leave it at the bottom of your main function.
+
+reaper.UpdateArrange()
+
+reaper.PreventUIRefresh(-1)
