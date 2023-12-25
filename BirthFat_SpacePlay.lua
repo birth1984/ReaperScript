f4d9cf905439f3cfe0027a76ddd08fb13c6cc862
Birth1984 <88521762@qq.com>
2023-12-26 06:31:21 +0800

    Update BirthFat_SpacePlay.lua

--- BirthFat_SpacePlay.lua
+++ BirthFat_SpacePlay.lua
@@ -9,9 +9,17 @@ reaper.ClearConsole()
 --reaper.ShowConsoleMsg(reaper.NamedCommandLookup("40044").." " ..tostring(reaper.Audio_IsRunning()))
 count_sel_items = reaper.CountSelectedMediaItems(0)
 if count_sel_items > 0 then
-  reaper.Main_OnCommand(reaper.NamedCommandLookup("_XENAKIOS_TIMERTEST1") , 0)
+  isRunning = reaper.Audio_IsRunning()
+  --reaper.ShowConsoleMsg(isRunning)
+  if isRunning == 1.0 then
+    reaper.Main_OnCommand(reaper.NamedCommandLookup("40044"), 0)
+  else 
+    reaper.Main_OnCommand(reaper.NamedCommandLookup("_XENAKIOS_TIMERTEST1"), 1)
+  end
+  -- reaper.Main_OnCommand(reaper.NamedCommandLookup("_XENAKIOS_TIMERTEST1") , 0)
 else 
   isRunning = reaper.Audio_IsRunning()
+  --reaper.ShowConsoleMsg(isRunning)
   if isRunning == 1.0 then
     reaper.Main_OnCommand(reaper.NamedCommandLookup("40044"), 0)
   else 
@@ -25,3 +33,4 @@ reaper.Undo_EndBlock("space play", -1) -- End of the undo block. Leave it at the
 reaper.UpdateArrange()
 
 reaper.PreventUIRefresh(-1)
+
